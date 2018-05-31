cc1886a98e9af3de72b51f7533a4d3ebbbb3f590
Incredible <wwwthomasattenberger13@gmail.com>
2018-05-31 12:52:27 +0200

    deleted retroclash for .net framework + added a setup for the server config

--- RetroClashCore/Configuration.cs
+++ RetroClashCore/Configuration.cs
@@ -34,7 +34,9 @@ namespace RetroClashCore
 
         [JsonProperty("server_port")] public int ServerPort = 9339;
 
-        [JsonProperty("update_url")] public string UpdateUrl = "https://RetroClashCore.pw/";
+        [JsonProperty("api_port")] public int ApiPort = 4800;
+
+        [JsonProperty("update_url")] public string UpdateUrl = "https://retroclash.pw/";
 
         public void Initialize()
         {
@@ -53,6 +55,7 @@ namespace RetroClashCore
                     RedisServer = config.RedisServer;
                     EncryptionKey = config.EncryptionKey;
                     ServerPort = config.ServerPort;
+                    ApiPort = config.ApiPort;
                 }
                 catch (Exception)
                 {
@@ -63,14 +66,66 @@ namespace RetroClashCore
             else
                 try
                 {
+                    Console.WriteLine("Welcome to RetroClash! Let's setup the server in just 8 steps.");
+
+                    Console.Write("STEP 1: Enter the update url for outdated clients: ");
+                    var updateUrl = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(updateUrl))
+                        UpdateUrl = updateUrl;
+
+                    Console.Write("STEP 2: Enter the content url for asset updates: ");
+                    var contentUrl = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(contentUrl))
+                        PatchUrl = contentUrl;
+
+                    Console.Write("STEP 3: Enter the mysql username: ");
+                    var mysqlUser = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(mysqlUser))
+                        MySqlUserId = mysqlUser;
+
+                    Console.Write("STEP 4: Enter the mysql password: ");
+                    var mysqlPassword = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(mysqlPassword))
+                        MySqlPassword = mysqlPassword;
+
+                    Console.Write("STEP 5: Enter the mysql server host: ");
+                    var mysqlServer = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(mysqlServer))
+                        MySqlServer = mysqlServer;
+
+                    Console.Write("STEP 6: Enter the name of the mysql database: ");
+                    var mysqlDatabase = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(mysqlDatabase))
+                        MySqlDatabase = mysqlDatabase;
+
+                    Console.Write("STEP 7: Enter the redis host: ");
+                    var redisHost = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(redisHost))
+                        RedisServer = redisHost;
+
+                    Console.Write("STEP 8: Enter the redis password: ");
+                    var redisPassword = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(redisPassword))
+                        RedisPassword = redisPassword;
+
                     File.WriteAllText("config.json", JsonConvert.SerializeObject(this, Formatting.Indented));
 
-                    Console.WriteLine("New configuration has been created. Restart server.");
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Setup completed. Restart the server now.");
                     Console.ReadKey();
                     Environment.Exit(0);
                 }
                 catch (Exception)
                 {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("Couldn't create config file.");
                     Console.ReadKey();
                     Environment.Exit(0);
